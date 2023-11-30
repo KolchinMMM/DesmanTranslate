10797b8ca3f26f8495c05921d0f50ccd98c63a60
ipaingo <szimenkova7@gmail.com>
2023-11-30 21:15:50 +0300

    editor page added

---
+++ src/pages/Editor.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+import Button from "react-bootstrap/Button"
+import { useNavigate } from "react-router-dom"
+
+import React, { setState, useEffect, useState, formData } from "react"
+
+
+export default function Create(){
+
+//   var id = 0 // ВРЕМЕННАЯ ФИГНЯ, ПОТОМ ПЕРЕДЕЛАТЬ
+//   let navigate = useNavigate(); 
+//   const routeChange = () =>{ 
+//     let path = '/project/:id'; 
+//     navigate(path);
+//   }
+
+  return (
+    <>
+      
+    </>
+  );
+}
