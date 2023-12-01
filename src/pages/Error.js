5c93072bfea958eb5190881357169db0bbea7562
ipaingo <szimenkova7@gmail.com>
2023-12-01 14:10:16 +0300

    user page added

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,6 @@
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import placeholder from "../images/placeholder.png";
 import { Container } from "react-bootstrap";
 
 export default function Notfound(){
